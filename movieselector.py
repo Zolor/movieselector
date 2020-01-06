2cb6b94c50df8f8a35b6b915396100635a7cf417
Erika Forslöf <erikaforslov@hotmail.com>
2020-01-06 21:18:01 +0100

    First Working Version

--- movieselector.py
+++ movieselector.py
@@ -3,26 +3,27 @@ import bs4
 import json
 import random
 
-imdb_url = input("Input your IMDB Watch List URL eg. https://www.imdb.com/user/ur7153790/watchlist ")
-res = requests.get(imdb_url)
-res.raise_for_status()
-soup = bs4.BeautifulSoup(res.text, "html.parser")
-js_elements = soup.find_all('script')
-js_text = None
-search_str = 'IMDbReactInitialState.push('
+def movieselector(imdb_url):
+	res = requests.get(imdb_url)
+	res.raise_for_status()
+	soup = bs4.BeautifulSoup(res.text, "html.parser")
+	js_elements = soup.find_all('script')
+	js_text = None
+	search_str = 'IMDbReactInitialState.push('
 
-for element in js_elements:
-    text = element.text
-    if search_str in text:
-        js_text = text.strip()
-        break
+	for element in js_elements:
+	    text = element.text
+	    if search_str in text:
+	        js_text = text.strip()
+	        break
 
-json_start = js_text.index(search_str) + len(search_str)
-json_text = js_text[json_start:-2]
-json_obj = json.loads(js_text[json_start:-2])
-imdb_lista = []
-for title in json_obj['titles']:
-    json_title = json_obj['titles'][title]
-    imdb_lista.append(json_title['primary']['title'])
+	json_start = js_text.index(search_str) + len(search_str)
+	json_text = js_text[json_start:-2]
+	json_obj = json.loads(js_text[json_start:-2])
+	imdb_lista = []
+	for title in json_obj['titles']:
+	    json_title = json_obj['titles'][title]
+	    imdb_lista.append(json_title['primary']['title'])
 
-print("Your next movie to see is: " + random.choice(imdb_lista))
+	random_movie = random.choice(imdb_lista)
+	return random_movie
